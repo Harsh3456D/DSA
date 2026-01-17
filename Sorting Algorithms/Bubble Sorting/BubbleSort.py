fe4d21a78801d170f78d87ad6db8401f3d8782bf
Harsh Dabas <harshdabas1314@gmail.com>
2026-01-17 22:49:57 +0530

    Fix bubble sort implementation and indexing

--- Sorting Algorithms/Bubble Sorting/BubbleSort.py
+++ Sorting Algorithms/Bubble Sorting/BubbleSort.py
@@ -2,7 +2,8 @@ arr:int = [3,2,4,5,8,11,8,45,22,65,32,87,54,99,12,34,21,56]
 arrLen:int = len(arr)
 
 def bubblesort(array:int) -> int:
-    for i in range(arrLen):
+    i = 0
+    for i in range(arrLen-i):
         for j in range(i+1,arrLen):
             if array[i] > array[j]:
                 array[i],array[j] = array[j],array[i]
@@ -13,3 +14,4 @@ def bubblesort(array:int) -> int:
     print(array)
 
 bubblesort(arr)
+
